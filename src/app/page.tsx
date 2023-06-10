7b5566b01c894b5e04c412d562d33eccc65c4f25
milkandhoney995 <harada.g17@gmail.com>
2023-06-10 12:46:40 +0900

    topを修正

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,10 +6,9 @@ export default function Home() {
   return (
     <main className={styles.main}>
       <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
+        <h1>
+          Look My Works
+        </h1>
       </div>
 
       <div className={styles.grid}>
