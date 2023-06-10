6d97414142109f7d8ffc2fdd0174a4a630451b5c
milkandhoney995 <harada.g17@gmail.com>
2023-06-11 08:32:10 +0900

    コンソールのエラー解消

--- src/app/calculator/page.tsx
+++ src/app/calculator/page.tsx
@@ -1,23 +1,30 @@
 'use client';
 
-import { useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { CalculatorClass } from "./main"
 import classes from "./page.module.scss"
 
 export default function Calculator() {
-  // const calculator = new CalculatorClass(previousOperandTextElement, currentOperandTextElement, '+', '+', 2, '+')
+  // const calculator = new CalculatorClass(previousOperandTextElementRef, currentOperandTextElementRef, '+', '+', 2, '+')
 
   // you can use when you are sure that the value is never null by adding the ! operator to the end of your statement
   // https://stackoverflow.com/questions/63520680/argument-of-type-htmlelement-null-is-not-assignable-to-parameter-of-type-el
-  const previousOperandTextElement = useRef<HTMLDivElement>(null);
-  const currentOperandTextElement = useRef<HTMLDivElement>(null)
+  const previousOperandTextElementRef = useRef<HTMLDivElement>(null);
+  const currentOperandTextElementRef = useRef<HTMLDivElement>(null)
+  const [previousOperandTextElement, setPreviousOperandTextElement] = useState<HTMLDivElement>()
+  const [currentOperandTextElement, setCurrentOperandTextElement] = useState<HTMLDivElement>()
   const [previousOperand, setPreviousOperand] = useState<string>('')
   const [currentOperand, setCurrentOperand] = useState<string>('')
 
-  console.log(previousOperandTextElement.current)
-  console.log(currentOperandTextElement.current)
+  useEffect(() => {
+    setPreviousOperandTextElement(previousOperandTextElementRef.current!)
+    setCurrentOperandTextElement(currentOperandTextElementRef.current!)
+    console.log(previousOperandTextElement)
+    console.log(currentOperandTextElement)
+  })
+
   const calculator = new CalculatorClass(
-    previousOperandTextElement.current!, currentOperandTextElement.current!,
+    previousOperandTextElement!, currentOperandTextElement!,
     previousOperand, currentOperand
   )
 
@@ -51,8 +58,8 @@ export default function Calculator() {
   return (
     <div className={classes.calculator}>
       <div className={classes.output}>
-        <div className={classes.previousOperand} ref={previousOperandTextElement}></div>
-        <div className={classes.currentOperand} ref={currentOperandTextElement}></div>
+        <div className={classes.previousOperand} ref={previousOperandTextElementRef}></div>
+        <div className={classes.currentOperand} ref={currentOperandTextElementRef}></div>
       </div>
       <button className={classes.spanTwo} onClick={() => allClear()}>AC</button>
       <button onClick={() => deleteButton()}>DEL</button>
