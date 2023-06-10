30e9b9e1eecbac59c8529f7d9071719d71f7a43b
milkandhoney995 <harada.g17@gmail.com>
2023-06-10 11:28:55 +0900

    calculator: CalculatorClass

---
+++ src/app/calculator/main.ts
@@ -0,0 +1,101 @@
+// https://www.typescriptlang.org/docs/handbook/declaration-files/do-s-and-don-ts.html
+export class CalculatorClass {
+  previousOperandTextElement: HTMLDivElement;
+  currentOperandTextElement:  HTMLDivElement;
+  currentOperand?: string
+  previousOperand?: string
+  number?: number
+  operation?: string | undefined
+
+  constructor(
+    previousOperandTextElement: HTMLDivElement,
+    currentOperandTextElement: HTMLDivElement,
+    currentOperand?: string, previousOperand?: string, number?: number,
+    operation?: string | undefined
+  ) {
+    this.previousOperandTextElement = previousOperandTextElement
+    this.currentOperandTextElement = currentOperandTextElement
+    this.currentOperand = currentOperand
+    this.previousOperand = previousOperand
+    this.number = number
+    this.operation = operation
+    this.clear()
+  }
+  clear() {
+    this.currentOperand = ''
+    this.previousOperand = ''
+    this.operation = undefined
+    console.log("delete")
+  }
+
+  delete() {
+    this.currentOperand = this.currentOperand!.toString().slice(0, -1)
+  }
+
+  appendNumber(number: string | number) {
+    if (number === '.' && this.currentOperand!.includes('.')) return
+    this.currentOperand = this.currentOperand!.toString() + number.toString()
+  }
+
+  chooseOperation(operation: string | undefined) {
+    if (this.currentOperand === '') return
+    if (this.previousOperand !== '') this.compute()
+    this.operation = operation
+    this.previousOperand = this.currentOperand
+    this.currentOperand = ''
+  }
+
+  compute() {
+    let computation: number
+    const prev = parseFloat(this.previousOperand!)
+    const current = parseFloat(this.currentOperand!)
+    if (isNaN(prev) || isNaN(current)) return
+    switch (this.operation) {
+      case '+':
+        computation = prev + current
+        break
+      case '-':
+        computation = prev - current
+        break
+      case '*':
+        computation = prev * current
+        break
+      case '÷':
+        computation = prev / current
+        break
+      default:
+          return
+
+    }
+    this.currentOperand = computation.toString()
+    this.operation = undefined
+    this.previousOperand = ''
+  }
+
+  getDisplayNumber(number: number) {
+    const stringNumber = number.toString()
+    const integerDigits = parseFloat(stringNumber.split('.')[0])
+    const decimalDigits = stringNumber.split('.')[1]
+    let integerDisplay
+    if (isNaN(integerDigits)) {
+        integerDisplay = ''
+    } else {
+        integerDisplay = integerDigits.toLocaleString('en', { maximumFractionDigits: 0 })
+    }
+    if (decimalDigits != null) {
+        return `${integerDisplay}.${decimalDigits}`
+    } else {
+        return integerDisplay
+    }
+  }
+  updateDisplay() {
+    this.currentOperandTextElement.innerText =
+        this.getDisplayNumber(Number(this.currentOperand))
+    if (this.operation != null) {
+        this.previousOperandTextElement.innerText =
+        `${this.getDisplayNumber(Number(this.previousOperand))} ${this.operation}`
+    } else {
+        this.previousOperandTextElement.innerText = ''
+    }
+  }
+}
--- src/app/calculator/page.tsx
+++ src/app/calculator/page.tsx
@@ -1,30 +1,64 @@
+'use client';
+
+import { useRef } from "react";
+import { CalculatorClass } from "./main"
 import classes from "./page.module.scss"
 
 export default function Calculator() {
+  // const calculator = new CalculatorClass(previousOperandTextElement, currentOperandTextElement, '+', '+', 2, '+')
+
+const previousOperandTextElement = useRef<HTMLDivElement>(null)
+const currentOperandTextElement = useRef<HTMLDivElement>(null)
+
+console.log(previousOperandTextElement.current?.innerHTML)
+// you can use when you are sure that the value is never null by adding the ! operator to the end of your statement
+// https://stackoverflow.com/questions/63520680/argument-of-type-htmlelement-null-is-not-assignable-to-parameter-of-type-el
+const calculator = new CalculatorClass(previousOperandTextElement.current!, currentOperandTextElement.current!)
+
+  function getNumber(buttonNumber: string) {
+    calculator.appendNumber(buttonNumber)
+    calculator.updateDisplay()
+  }
+ function operate(buttonText: string) {
+    calculator.chooseOperation(buttonText)
+    calculator.updateDisplay()
+}
+  function equal() {
+    calculator.compute()
+    calculator.updateDisplay()
+  }
+  function allClear(){
+    calculator.clear()
+    calculator.updateDisplay()
+  }
+  function deleteButton() {
+    calculator.delete()
+    calculator.updateDisplay()
+  }
   return (
     <div className={classes.calculator}>
       <div className={classes.output}>
-        <div data-previousOperand className={classes.previousOperand}></div>
-        <div data-current-operand className={classes.currentOperand}></div>
+        <div className={classes.previousOperand} ref={previousOperandTextElement}></div>
+        <div className={classes.currentOperand} ref={currentOperandTextElement}></div>
       </div>
-      <button data-all-clear className={classes.spanTwo}>AC</button>
-        <button data-delete>DEL</button>
-        <button data-operation>÷</button>
-        <button data-number>1</button>
-        <button data-number>2</button>
-        <button data-number>3</button>
-        <button data-operation>*</button>
-        <button data-number>4</button>
-        <button data-number>5</button>
-        <button data-number>6</button>
-        <button data-operation>+</button>
-        <button data-number>7</button>
-        <button data-number>8</button>
-        <button data-number>9</button>
-        <button data-operation>-</button>
-        <button data-number>.</button>
-        <button data-number>0</button>
-        <button data-equals className={classes.spanTwo}>=</button>
+      <button className={classes.spanTwo} onClick={() => allClear()}>AC</button>
+        <button onClick={() => deleteButton()}>DEL</button>
+        <button onClick={() => operate('÷')}>÷</button>
+        <button onClick={() => getNumber('1')}>1</button>
+        <button onClick={() => getNumber('2')}>2</button>
+        <button onClick={() => getNumber('3')}>3</button>
+        <button onClick={() => operate('*')}>*</button>
+        <button onClick={() => getNumber('4')}>4</button>
+        <button onClick={() => getNumber('5')}>5</button>
+        <button onClick={() => getNumber('6')}>6</button>
+        <button onClick={() => operate('+')}>+</button>
+        <button onClick={() => getNumber('7')}>7</button>
+        <button onClick={() => getNumber('8')}>8</button>
+        <button onClick={() => getNumber('9')}>9</button>
+        <button onClick={() => operate('-')}>-</button>
+        <button onClick={() => getNumber('.')}>.</button>
+        <button onClick={() => getNumber('0')}>0</button>
+        <button className={classes.spanTwo} onClick={() => equal()}>=</button>
     </div>
   )
 }
