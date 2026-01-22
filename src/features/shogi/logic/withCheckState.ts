c030f9fb894d894853c5ce0012fea45c23218b80
milkandhoney995 <harada.g17@gmail.com>
2026-01-22 19:26:59 +0900

    withCheckStateでisSentePieceを使わないように修正

--- src/features/shogi/logic/withCheckState.ts
+++ src/features/shogi/logic/withCheckState.ts
@@ -1,7 +1,6 @@
 import { ShogiState } from '@/features/shogi/state/shogiState';
 import { isKingInCheck } from './isKingInCheck';
 import { findKingPosition } from './findKingPosition';
-import { isSentePiece } from './shogiHelpers';
 
 /**
  * 局面が確定したら、王手状態などを再評価する
@@ -9,8 +8,12 @@ import { isSentePiece } from './shogiHelpers';
  * @param state 現在の将棋の状態
  * @returns 王手情報を付与した将棋の状態
  */
-export const withCheckState = (state: ShogiState): ShogiState => ({
-  ...state,
-  isInCheck: isKingInCheck(state.board, isSentePiece(state.turn)),
-  kingPosition: findKingPosition(state.board, state.turn),
-});
+export const withCheckState = (state: ShogiState): ShogiState => {
+  const isSenteTurn = state.turn === 'sente';
+
+  return {
+    ...state,
+    isInCheck: isKingInCheck(state.board, isSenteTurn),
+    kingPosition: findKingPosition(state.board, state.turn),
+  };
+};
